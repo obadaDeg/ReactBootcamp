926544e4f0aa0c654d762b044b01aa4440b94177
obadaDeg <animayoloteer@gmail.com>
2023-09-09 08:55:39 +0300

    loan-app

---
+++ loan-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MainBar from "./Components/MainBar"
+import Message from './Components/Message';
+
+function App() {
+  return (
+    <div className="App">
+      <MainBar></MainBar>
+            
+    </div>
+  );
+}
+
+export default App;
